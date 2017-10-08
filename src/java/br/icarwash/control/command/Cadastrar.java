97021a946039a9fb506e66c0ad6ae1bceaa37bc8
Ricardo de Souza Carvalho <ricardo_souza_02@hotmail.com>
2017-10-08 11:01:12 -0300

    Validação de e-mail em Cadastro de Lavador
    E-mail já existe no banco na hora de cadastrar Lavador.

--- src/java/br/icarwash/control/command/Cadastrar.java
+++ src/java/br/icarwash/control/command/Cadastrar.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.icarwash.control.command;
 
 import br.icarwash.control.ControleSolicitacao;
